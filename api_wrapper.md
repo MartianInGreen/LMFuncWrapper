99b69c054778c7f3fd08b1d8ca9e7ce3359ee011
Hannah <github@rennersh.eu>
2024-03-17 11:42:32 +0100

    Significant rewrite
    - Switch from JSON to XML
    - Base on "Action" Type agent

--- api_wrapper.md
+++ api_wrapper.md
@@ -1,66 +1,89 @@
-You are a JSON-Agent, you always respond with **only** **valid JSON**.
-You **always** follow the [[Template]] and subtemplates for *text_message* and *tool_call*.
-You **must only** call a tool if it JSON structure of it has been given to you under the TOOLS section, DO NOT MAKE UP TOOLS! DO NOT CALL TOOLS THAT DO NOT EXCIST! TELL THE USER IF YOU CAN'T DO SOMETHING!
+You are an xml agent. You have multiple options to choose from, however you're only ever allowed to write within the xml tags.
 
-**TEMPLATES**
--------------
-If there is an object with a "type" property, the parent property should be written with that type!
+**Instructions:**
+-----------------
+A few key things to keep in mind, you **have to follow** these instructions:
 
-**[[Template]]**
+- Only include a <tool_call> if it's needed to answer the question. It's okay if you don't need to use a tool.
+- Always concider which tool is the best tool for the job.
+- Do not write the <tool_response> yourself - this will be returned by the tool after your <tool_call>.
+- When you receive a <tool_response>, use the information in it to help formulate your final answer. - - Don't just repeat the <tool_response> word-for-word.
+- Only call tools that exist in the toolkit below. Do not make up tool names.
+- If you don't have the right tool to answer a question, just say so! Don't try to call a non-existent tool.
+- Generally you should follow the order, <though>, <message> (, optionally <tool_call>) and then repeat.
+- Never write outside of the <though>, <message>, or <tool_call> tags!
+- Don't call any tool in an infinte loop, make use of the results you got!
+
+**TAGS**
+--------
+
+<avalible_tags>
+These are your high level tags you can use:
+- <message>: A message you send to the user
+- <thought>: An internal thought to think about what to do next or similar. 
+- <tool_call>: Call an external tool/plugin to help you expand your capabilities.
+</avalible_tags>
+
+<tag_format>
+Message format:
+<message>Your message to the user.<message>
+
+Thought format:
+<thought>Your internal thought.<thought>
+
+Tool format:
+<tool_call>
+    <tool_name type="str">Name of the tool you want to call</tool_name>
+    <parameters type="dict">
+        Parameters here!
+    </parameters>
+</tool_call>
+</tag_format>
+
+<parameter_example>
+<!--Tool description properties-->
+<properties type="dict">
+    <!--From each propertie, you can see the type the actuall call has to be in the <type> tag. For example here the type of <query> would be string. The description is only for you so you know what you have to put in <query type="str">.-->
+    <query type="dict">
+        <type type="str">string</type>
+        <description type="str">The query to send to the API</description>
+    </query>
+</properties>
+
+<!--Resulting parameters format, this is how you have to write it!-->
+<parameters type="dict">
+    <query type="str">Example Query</query>
+</parameters>
+
+The same way a JSON propertie defined as this:
 ```json
 {
-    "action": {
+    "query": {
         "type": "string",
-        "enum": ["text_message", "tool_call"]
-    },
-    "action_data": {
-        "type": "object",
-        "description": "JSON Object in the form of either the text_message_template or tool_call_template"
-    }
-}
-``` 
-
-**[[text_message_template]]** (Use this **every time** you want to send just text!)
-```json
-{
-    "message": {
-        "type": "string",
-        "description": "Message to send to the user."
-    }
+        "description": "The query to send to the API"
+    } 
 }
 ```
 
-**[[tool_call_template]]**
+Would be interpreted as:
 ```json
 {
-    "name": {
-        "type": "string",
-        "description": "Name of the function to call. ONLY CALL FUNCTIONS YOU ARE CERTAIN EXCISTS"
-    },
-    "arguments"{
-        "type": "object",
-        "description": "Arguments to pass to the function."
-    }
+    "query": "Query to the api."
 }
 ```
+
+You're just working with XML, but the same principles that apply to JSON still apply here.
+</parameter_example>
 
 **TOOLS**
 ---------
 
+Here are the available tools in your toolkit:
 {{tools}}
 
+You only have the tools above! 
+DO NOT MAKE UP TOOLS! DO NOT CALL TOOLS THAT DO NOT EXCIST! 
 
-**EXAMPLE**
 ---------
-[[example_call]]
-```json
-{
-    "action": "text_message",
-    "action_data": {
-        "message": "Sure! How can I help you?"
-    }
-}
-```
 
-Remember to respond with a markdown code snippet of a json blob with a **single** action, and NOTHING else! 
-Make sure your response **always** fits the provided structure, it will fail otherwise! (Even when just sending text)
+FOLLOW THE INSTRUCTIONS ABOVE CAREFULLY!
--- api_wrapper.py
+++ api_wrapper.py
@@ -1,5 +1,8 @@
 from openai import OpenAI
-import os, json, re, uuid
+from dicttoxml import dicttoxml
+import xmltodict
+import os, json, uuid, re
+#from rich import print
 
 def auth(headers):
     if headers["x-api-key"] != os.getenv("SERVICE_API_KEY"):
@@ -17,16 +20,109 @@ def load_params(event):
         raise Exception("Error loading parameters: " + str(e))
     
 def add_system_message(messages, functions):
-    # Read api_wrapper.md
-    with open("api_wrapper.md", "r") as file:
+     # Read api_wrapper.md
+    with open("instructionsV2.md", "r") as file:
         content = file.read()
         content = content.replace("{{tools}}", str(functions))
 
-    # Insert
-    messages.append({"role": "system", "content": content})
+    # Insert system message
+    # if last message was a tool response, insert system message before it
+    if messages[-1]["role"] == "tool":
+        messages.insert(-1, {"role": "system", "content": content})
+    else:
+        messages.append({"role": "system", "content": content})
     #print(messages)
+
+    # Replace tool response with system message
+    for message in messages:
+        if message["role"] == "tool":
+            message["role"] = "user"
+            message["content"] = "*SYSTEM MESSAGE*\nResponse from the tool you just called.\n<tool_response source=" + message["name"] + ">\n" + message["content"] + "\n</tool_response>"
+
     return messages
 
+def parse_tool_json(raw_json):
+    tool_name = raw_json["tool_call"]["tool_name"]["#text"]
+    args = {}
+
+    for arg_name, arg_value in raw_json["tool_call"]["parameters"].items():
+        if isinstance(arg_value, dict):
+            if '#text' in arg_value:
+                args[arg_name] = arg_value['#text']
+            else:
+                args[arg_name] = arg_value
+        else:
+            if arg_name != "@type":
+                args[arg_name] = arg_value
+
+    tool_calls = [{
+        'id': str(uuid.uuid4()),
+        'function': {
+            'name': tool_name,
+            'arguments': json.dumps(args)
+        },
+        'type': 'function'
+    }]
+    
+    return tool_calls
+
+def build_return_json(message, tool_calls, id, created, model, stream, finish_reason=None):
+    if False:
+        object = "chat.completion"
+
+        # Normal messages
+        if tool_calls == None:
+            messages = [{
+                'index': 0,
+                'delta': {
+                    'role': message["role"],
+                    'content': message["content"]
+                    },
+                'finish_reason': finish_reason
+            }]
+        if tool_calls != None:
+            messages = [{
+                'index': 0,
+                'delta': {
+                    'role': message["role"],
+                    'content': message["content"],
+                    'tool_calls': tool_calls
+                },
+                'finish_reason': finish_reason
+            }]
+
+    else:
+        object = "chat.completion"
+
+        # Normal messages
+        if tool_calls == None:
+            messages = [{
+                'message': {
+                    'role': message["role"],
+                    'content': message["content"]
+                },
+            }]
+        # Tool messages
+        else:
+            messages = [{
+                'message': {
+                    'role': message["role"],
+                    'content': message["content"],
+                    'tool_calls': tool_calls
+                }
+            }]
+    
+
+    return_data = {
+        'choices': messages,
+        'id': id,
+        'object': object,
+        'created': created,
+        'model': model,
+    }
+
+    return return_data
+    
 def lambda_handler(event, context):
     body = load_params(event)
     print(body)
@@ -36,10 +132,9 @@ def lambda_handler(event, context):
         messages = body["messages"]
         try: 
             functions = body["tools"]
-            print(functions)
+            functions = dicttoxml(functions, custom_root="tools", xml_declaration=False)
         except:
-            functions = "No tools available."
-            print(functions)
+            functions = "No tools available. Do not call any!"
         try:
             streaming = body["stream"]
         except:
@@ -48,14 +143,13 @@ def lambda_handler(event, context):
         # Add system message
         messages = add_system_message(messages, functions)
 
-        # Modify tool response messages
-        for message in messages:
-            if message["role"] == "tool":
-                message["role"] = "system"
-                message["content"] = "Tool response:\n\n" + message["content"]
+        print(messages)
+
     except Exception as e:
         raise Exception("Error loading parameters: " + str(e))
     
+    # Call OpenRouter API
+
     try: 
         client = OpenAI(
             base_url="https://openrouter.ai/api/v1",
@@ -64,229 +158,76 @@ def lambda_handler(event, context):
 
         completion = client.chat.completions.create(
             model=model,
-            messages=messages
+            messages=messages,
+            stream=streaming,
+            #stop=["</tool_call>"]
         )
+
+        print(completion)
+        tool_call_json = None
+
+        if streaming == True: 
+            tool_call_xml = ""
+            start_tool_call = False
+            tool_call_json = None
+            message = None
+            finish_reason = None
+            for chunk in completion: 
+                #print(chunk)
+                if chunk.choices[0].finish_reason is not None: 
+                    print("\nSTOP")
+                    finish_reason = chunk.choices[0].finish_reason
+                if "<tool" in chunk.choices[0].delta.content and start_tool_call == False:
+                    tool_call_xml = tool_call_xml + chunk.choices[0].delta.content
+                    start_tool_call = True
+                elif start_tool_call == True:
+                    tool_call_xml = tool_call_xml + chunk.choices[0].delta.content
+                    if "</tool_call>" in tool_call_xml:
+                        start_tool_call = False
+                        tool_call_json = xmltodict.parse(tool_call_xml)
+                elif tool_call_json == None:
+                    if message == None:
+                        message = ""
+                    message = message + chunk.choices[0].delta.content
+                    #print(build_return_json(chunk.choices[0].delta, tool_call_json, chunk.id, chunk.created, model, streaming, finish_reason))
+            
+            if tool_call_json != None:
+                print("Found tool call!")
+                parsed_json = parse_tool_json(tool_call_json)
+            else:
+                parsed_json = None
+
+            message = {
+                "role": "assistant",
+                "content": message
+            }
+
+            json_return = build_return_json(message, parsed_json, chunk.id, chunk.created, model, streaming, finish_reason)
+            return json_return
+
+        if streaming == False:
+            message = {
+                "role": "assistant",
+                "content": re.sub(r'(<tool_call>)[\s\S]*(<\/tool_call>)[\s\S]*', '', completion.choices[0].message.content)
+            }
+
+
+            message["content"] = message["content"].replace("<message>\n", "").replace("<message>", "")
+            message["content"] = message["content"].replace("</message>\n", "").replace("</message>", "")
+
+            message["content"] = message["content"].replace("<thought>\n", "*").replace("<thought>", "*")
+            message["content"] = message["content"].replace("</thought>\n", "*").replace("</thought>", "*")
+
+            # Find tool calls using regex
+            tool_call_xml = re.search(r'(<tool_call>)[\s\S]*(<\/tool_call>)', completion.choices[0].message.content)
+            if tool_call_xml != None:
+                tool_call_xml = tool_call_xml.group()
+                tool_call_json = xmltodict.parse(tool_call_xml)
+                parsed_json = parse_tool_json(tool_call_json)
+            else:
+                parsed_json = None
+
+            json_return = build_return_json(message, parsed_json, completion.id, completion.created, model, streaming)
+            return json_return
     except Exception as e:
         raise Exception("Error calling API: " + str(e))
-
-    # Determine action type
-    try: 
-        print(completion)
-        raw_response = completion.choices[0].message.content
-        converted_json_string = False
-        #print(response)
-        try:
-            json_string = re.search('```json\n(.*?)\n```', raw_response , re.DOTALL).group(1)
-        except:
-            try:
-                # Search for JSON string without code block -> Message starts with {
-                no_nl = raw_response.replace('{\n', '{').replace('\n}', '}')
-                no_nl = no_nl.replace('}\n', '}').replace('\n{', '{')
-
-                if no_nl.startswith('[['):
-                    # Remove leading [[some_text]]
-                    print("Removing leading [[some_text]]")
-                    no_nl = re.sub(r'^\[\[.*?\]\]', '', no_nl)
-                if not no_nl.startswith('{'):
-                    print("Removing leading text")
-                    # Match any leading text before the JSON string
-                    no_nl = re.sub(r'^[^{]*' '', no_nl)
-
-                match = re.search(r'^\s*?({.*})', no_nl, re.DOTALL)
-                if match:
-                    json_string = match.group(1)
-                    response = json.loads(json_string)
-                    converted_json_string = True
-                    print("Converted JSON string in first alternative step.")
-                else:
-                    print("Error parsing response: No JSON string found in first alternative step.")
-                    raise Exception("Error parsing response: No JSON string found")
-            except:
-                # Sometimes raw response looks smth. like this: pondera({"query": "latest news about OpenAI", "type": "deep", "focus": "web", "country": "US", "freshness": "week"})
-                match = re.search(r'(\w+)\((.*?)\)', raw_response, re.DOTALL)
-
-                if match:
-                    function_name = match.group(1)
-                    function_args = match.group(2)
-
-                    json_string = '{"action": "tool_call", "action_data": {"name": "' + function_name + '", "arguments": ' + function_args + '}}'
-                try:
-                    response = json.loads(json_string)
-                    converted_json_string = True
-                except:
-                    pass 
-
-                if not converted_json_string:
-                    json_string = '{"action": "text_message", "action_data": {"message": ' + json.dumps(raw_response) + '}}'
-        
-        if not converted_json_string:
-            try:
-                # Remove any leading or trailing characters before { and after the last } (including normal text)
-                json_string = re.sub(r'^.*?({.*}).*$', r'\1', json_string, flags=re.DOTALL)
-
-                response = json.loads(json_string)
-            except Exception as e:
-                print("Response:")
-                print(raw_response)
-                print("JSON string:")
-                print(json_string)
-                raise Exception("Error parsing response: " + str(e))
-
-        print(json_string)
-
-        try: 
-            action = response["action"]
-            # See if action is string
-            if isinstance(action, str):
-                action = action.lower()
-            else:
-                action = str(action["type"]).lower()
-        except:
-            raise Exception("Error parsing response: No action found")
-        
-        # Check if action is valid
-        if action not in ["text_message", "tool_call"]:
-            raise Exception("Error parsing response: Invalid action")
-        
-        # Get action data
-        try:
-            action_data = response["action_data"]
-        except:
-            try:
-                alt_action_data = response["action"]["action_data"]
-            except:
-                raise Exception("Error parsing response: No action data found")
-
-        if action == "text_message": 
-            try: 
-                response_message = action_data["message"]
-            except:
-                try: 
-                    response_message = alt_action_data["message"]
-                except:
-                    raise Exception("Error parsing response: No message found")
-        if action == "tool_call":        
-            try: 
-                tool_name = action_data["name"]
-                tool_args = action_data["arguments"]
-            except:
-                try: 
-                    tool_name = alt_action_data["name"]
-                    tool_args = alt_action_data["arguments"]
-                except:
-                    try:
-                        tool_name = response["action"]["name"]
-                        tool_args = response["action"]["arguments"]
-                    except:
-                        raise Exception("Error parsing response: No tool data found")
-
-
-        # If action is a message, return it
-        if action == "text_message":
-            completion.choices[0].message.content = response_message
-            
-            return_data = {
-                'id': completion.id,
-                'object': completion.object,
-                'created': completion.created,
-                'model': completion.model,
-                'usage': {
-                    # CompletionUsage(completion_tokens=68, prompt_tokens=500, total_tokens=568, total_cost=0.00252)
-                    'completion_tokens': completion.usage.completion_tokens,
-                    'prompt_tokens': completion.usage.prompt_tokens,
-                    'total_tokens': completion.usage.total_tokens,
-                    'total_cost': completion.usage.total_cost    
-                },
-                'choices': [{
-                    'message': {
-                        'role': completion.choices[0].message.role,
-                        'content': completion.choices[0].message.content
-                    },
-                    'finish_reason': 'stop',
-                }]
-            }
-
-            if streaming == True:
-                return_data = {
-                    "choices": [{
-                        "index": 0,
-                        "delta": {
-                            "role": completion.choices[0].message.role,
-                            "content": completion.choices[0].message.content
-                        },
-                        "finish_reason": "stop"
-                    }],
-                    "id": completion.id,
-                    "object": completion.object,
-                    "created": completion.created,
-                    "model": completion.model,
-                }
-
-                print("data: " + json.dumps(return_data))
-                return json.dumps(return_data)
-
-            print(json.dumps(return_data))
-
-            return return_data
-        elif action == "tool_call":
-            completion.choices[0].message.content = None
-
-            return_data = {
-                'id': completion.id,
-                'object': completion.object,
-                'created': completion.created,
-                'model': completion.model,
-                'usage': {
-                    # CompletionUsage(completion_tokens=68, prompt_tokens=500, total_tokens=568, total_cost=0.00252)
-                    'completion_tokens': completion.usage.completion_tokens,
-                    'prompt_tokens': completion.usage.prompt_tokens,
-                    'total_tokens': completion.usage.total_tokens,
-                    'total_cost': completion.usage.total_cost    
-                },
-                'choices': [{
-                    'message': {
-                        'role': completion.choices[0].message.role,
-                        'content': completion.choices[0].message.content,
-                        'tool_calls': [{
-                            'id': str(uuid.uuid4()),
-                            'function': {
-                                'arguments': json.dumps(tool_args),
-                                'name': tool_name,
-                            },
-                            'type': 'function'
-                        }]
-                    },
-                    'finish_reason': completion.choices[0].finish_reason,
-                }]
-            }
-
-            if streaming:
-                return_data = {
-                    "choices": [{
-                        "index": 0,
-                        "delta": {
-                            "role": completion.choices[0].message.role,
-                            "content": completion.choices[0].message.content,
-                            "tool_calls": [{
-                                "id": str(uuid.uuid4()),
-                                "function": {
-                                    "arguments": json.dumps(tool_args),
-                                    "name": tool_name,
-                                },
-                                "type": "function"
-                            }]
-                        },
-                        "finish_reason": completion.choices[0].finish_reason
-                    }],
-                    "id": completion.id,
-                    "object": completion.object,
-                    "created": completion.created,
-                    "model": completion.model,
-                }
-
-            print(return_data)
-            return return_data
-        
-        print(response)
-    except Exception as e:
-        raise Exception("Error parsing response: " + str(e))
